87375532d155be534411a25eadb75051206a29d9
Halina Tulchii <halendeihalyna@gmail.com>
2023-04-01 13:14:39 +0300

    update

--- src/index.js
+++ src/index.js
@@ -7,25 +7,22 @@ import LoadMoreBtn from './js/load-more-btn';
 
 
 
-
-
-
 const refs = {
     searchForm: document.querySelector('#search-form'),
     galleryBox: document.querySelector('.gallery'),
   };
   
-  const loadMoreBtn = new LoadMoreBtn({
+const loadMoreBtn = new LoadMoreBtn({
     selector: '.load-more',
     hidden: true,
   });
   
-  const apiService = new ApiService();
+const apiService = new ApiService();
   
   refs.searchForm.addEventListener('submit', onSearchFormSubmit);
   loadMoreBtn.refs.button.addEventListener('click', onLoadMoreClick);
   
-  const lightbox = new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
     captionDelay: 250,
     captionsData: 'alt',
   });
@@ -38,8 +35,7 @@ const refs = {
     apiService.query = e.currentTarget.elements.searchQuery.value;
     
     console.log(apiService.page);
-    apiService.resetPage();
-    console.log(await apiService.fetchGallery())
+      apiService.resetPage();
     const { hits, totalHits } = await apiService.fetchGallery();
   
     if (totalHits > 5) {
@@ -98,8 +94,42 @@ const refs = {
     }
   }
   
-  function appendGalleryMarkup(photo) {
-    refs.galleryBox.insertAdjacentHTML('beforeend', galleryMarkup(photo));
+  function appendGalleryMarkup(elements) {
+
+    const markup = elements.map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+  <a class="gallery-link" href="${largeImageURL}">
+  <img  loading="lazy"
+  src="${webformatURL}" 
+  sourceAttr="${largeImageURL}"
+  alt="${tags}" width="350px" height="200px"/>
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes </b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views </b>${views}
+    </p>
+    <p class="info-item">
+      <b>Coments </b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads </b>${downloads}
+    </p>
+  </div>
+</div>`;
+      }
+    ).join('');
     lightbox.refresh();
   }
   
